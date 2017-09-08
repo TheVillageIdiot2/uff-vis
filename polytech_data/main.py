732413107578c351fed0aaa7d7b6fbe0aff4199f
Jacob Henry <jakehenry.tn@gmail.com>
2017-09-07 21:29:10 -0600

    Saving

--- polytech_data/main.py
+++ polytech_data/main.py
@@ -6,7 +6,7 @@ from tabulate import tabulate
 
 #Constants for rendering
 FPS = 30
-SIZE = (700, 500)
+SIZE = (500, 500)
 
 #Colors
 WHITE = (255,255,255)
@@ -19,75 +19,101 @@ TYPE_DISPLACEMENT = 58
 ID2_X_DISPLACEMENT = "Response X  Displacement"
 ID2_Y_DISPLACEMENT = "Response Y  Displacement"
 ID2_Z_DISPLACEMENT = "Response Z  Displacement"
-DATA_X_KEY = 'y' #For whatever reson the X axis is blank for positions. x corresponds then to y, and y to z
-DATA_Y_KEY = 'z'
 
 #Don't print these keys. Junk data
 IGNORE_LIST = ["unit_exp", "axis_lab", "func_type", "ver_num", "ref_ent_name", "orddenom"]
+GROWTH_SCALAR = 50_000
+DISPLACEMENT_POINTS = 10_000
 
 #Just holds xyz data
 class Vec3:
-    def __init__(self):
-        self.__init__(0,0,0)
-
-    def __init__(self, x, y, z):
+    def __init__(self, x=0, y=0, z=0):
         self.x = x
         self.y = y
         self.z = z
 
+    def tuple(self):
+        return self.x, self.y, self.z
+
+    def copy(self):
+        return Vec3(self.x, self.y, self.z)
+
+    def __add__(self, other):
+        if isinstance(other, Vec3):
+            return Vec3(self.x + other.x, self.y + other.y, self.z + other.z)
+        else:
+            raise Exception("You can't add other shit to a vec3, numbnuts")
+
+
 #Class which holds the initial position and all displacements
 #of a single vertex
-class VertexData:
+class NodeData:
     @classmethod
-    def get_base_position(data_set, node_id):
+    def get_base_position(cls, data_set, target_node_id):
+        '''
+        Returns a Vec3 containing the specified node's base position
+        Returns None on failure
+        '''
         for d in data_set:
             if d.get('type') == TYPE_VERTEX:
-                #Once found, get index of given node id
-                vert_index = d.node_nums.index(node_id)
-                vert_x = d.x[vert_index]
-                vert_y = d.y[vert_index]
-                vert_z = d.z[vert_index]
+                #Once found vertex dataset, search for the correct node index
+                for node_index, node_id in enumerate(d.get('node_nums')):
+                    if node_id == target_node_id:
+                        vert_x = d.get('x')[node_index]
+                        vert_y = d.get('y')[node_index]
+                        vert_z = d.get('z')[node_index]
 
-                #Construct base position
-                return Vec3(vert_x, vert_y, vert_z)
+                        #Construct base position
+                        vert = Vec3(vert_x, vert_y, vert_z)
+                        return vert
+
+
+        #If failed to find, just give up and go home
         return None
 
     @classmethod
-    def get_displacements(data_set, node_id):
+    def get_displacements(cls, data_set, node_id):
+        '''
+        Returns (displacements, times), where
+        - displacements = is a list of Displacement objects,
+        - times = a list of decimal values representing times
+        Both lists are of equivalent length, and correspond 1:1 in elements.
+        Returns None,None on failure
+        '''
         #First gather the data from data_set
-        disp_x, disp_y, disp_z = None, None, None
+        disp_x, disp_y, disp_z, disp_times = None, None, None, None
         for d in data_set:
-            if d.get('type') == TYPE_DISPLACEMENT and d.get('ref_node') == node_id:
+            if d.get('type') == TYPE_DISPLACEMENT and d.get('rsp_node') == node_id:
                 id2 = d.get('id2')
-                if id2 == ID2_X_DISPLACEMENT:
+                if      id2 == ID2_X_DISPLACEMENT:
                     disp_x = d.get('data')
-                elif id2 == ID2_Y_DISPLACEMENT:
+                elif    id2 == ID2_Y_DISPLACEMENT:
                     disp_y = d.get('data')
-                elif id2 == ID2_Z_DISPLACEMENT:
+                elif    id2 == ID2_Z_DISPLACEMENT:
                     disp_z = d.get('data')
+                    disp_times = d.get('x') # COuldve been anywhere else but whatever
 
         #If all goes well we will return a valid list, else nothing
-        if not (disp_x and disp_y and disp_z):
-            return None
+        if (disp_x is not None) and (disp_y is not None) and (disp_z is not None) and (disp_times is not None):
+            #Combine them
+            return [Vec3(x,y,z) for x,y,z in zip(disp_x, disp_y, disp_z)], disp_times
         else:
-            #Comebine them
-            return [Vec3(x,y,z) for x,y,z in zip(disp_x, disp_y, disp_z)]
-
-
+            #Return nothing if invalid
+            return None, None
 
     #Constructs the vertex data out of the givend ata set
     def __init__(self, data_set, node_id):
         #Find vertex data set
-        self.base_postion = VertexData.get_base_position(data_set, node_id)
+        self.base_postion = NodeData.get_base_position(data_set, node_id)
 
         #If didn't find vertex data set, fail
         if not self.base_postion:
             raise Exception("Failed to find vertex position for node_id {}".format(node_id))
 
         #Now find displacements
-        self.displacements = VertexData.get_displacements(data_set, node_id)
-        if not (disp_x and disp_y and disp_z):
-            raise Exception("Failed to find displacements for node_id {}".format(node_id))
+        self.displacements, self.disp_times = NodeData.get_displacements(data_set, node_id)
+        if (self.displacements is None) or (self.disp_times is None):
+            raise Exception("Failed to find displacements/times for node_id {}".format(node_id))
 
 #Pretty prints all the keys in a dict, ignoring those keys in IGNORE_LIST
 def print_keys(elem):
@@ -103,34 +129,40 @@ def print_keys(elem):
         print(tabulate(kvp, headers=['key', 'value']))
         input()
 
-def draw_point(surface, x,y):
-    radius = 2
-    pygame.draw.circle(surface, BLACK, (x, y), radius)
+def draw_point(surface, x, y, z):
+    radius = 2 + abs(x)
+    pygame.draw.circle(surface, BLACK, (y, z), radius)
 
 #Converts from the [-1,1] space to SIZE
-def to_screen_space(x,y):
+def to_screen_space(x,y,z):
     #Convert to fraction of [-1,1]
-    fx = (x + 1) / 2
     fy = (y + 1) / 2
+    fz = (z + 1) / 2
     
     #Convert to rough screen space
-    sx = fx * SIZE[0]
-    sy = fy * SIZE[1]
+    sy = fy * SIZE[0]
+    sz = fz * SIZE[1]
 
-    return (int(sx), int(sy))
+    #Bound to screen
+    sy = max(0, min(sy, SIZE[0] - 1))
+    sz = max(0, min(sz, SIZE[1] - 1))
 
-def get_displacement(data_set, node_id, id_key):
-    displacement_sets = [e for e in data_set if e.get('type') == TYPE_DISPLACEMENT]
-    node_sets = [e for e in displacement_sets if e.get('rsp_node') == node_id]
-    needed_sets = [e for e in node_sets if e.get('id2') == id_key]
-    if len(needed_sets):
-        return needed_sets[0]
+    return int(x*GROWTH_SCALAR), int(sy), int(sz)
+
+#Draws a single NodeData object
+def draw_node(surface, node_data, disp_index=-1):
+    if disp_index == -1:
+        displacement = Vec3()
     else:
-        raise Exception("Couldn't find {} for node {}".format(id_key, node_id))
+        displacement = node_data.displacements[disp_index]
+    #Build position of node at given time
+    pos = (node_data.base_postion + displacement).copy()
 
-def draw_vertex(vertex_dataset, disp_set_x, disp_set_y, disp_set_z):
-    pass
+    #Convert x and y to screen space
+    screen_pos = Vec3(*to_screen_space(*pos.tuple()))
 
+    #Draw
+    draw_point(surface, *screen_pos.tuple())
 
 def main(filename):
     #Open the UFF data
@@ -140,14 +172,11 @@ def main(filename):
 
     #Print out all our data
     for datum in data_set:
-        print_keys(datum)
+        #print_keys(datum)
+        pass
 
     #Get vertices
-    point_data_sets = [d for d in data_set if d.get('type') == TYPE_VERTEX]
-    vertex_dataset = point_data_sets[0]
-
-    #Get displacements
-    displacements = [d for d in data_set if d.g
+    node_data = [NodeData(data_set, node_num) for node_num in range(1,25)]
 
     #Init pygame
     pygame.init()
@@ -156,6 +185,7 @@ def main(filename):
 
     #Main loop
     done = False
+    i=0
     while not done:
         #Handle events
         for event in pygame.event.get():
@@ -170,11 +200,10 @@ def main(filename):
         screen.fill(WHITE)
     
         #Draw all points to screen.
-        points_x = point_data[DATA_X_KEY]
-        points_y = point_data[DATA_Y_KEY]
-        for point in zip(points_x, points_y):
-            point = to_screen_space(*point)
-            draw_point(screen, *point)
+        i = (i+10) % 10000
+        print(i)
+        for node in node_data:
+            draw_node(screen, node,i)
 
         #Push all rendered data to screen
         pygame.display.flip()
