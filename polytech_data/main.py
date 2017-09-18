4de3078ca43724cffb65f67a09f69b2f1ed831a8
Unknown <jakehenry.tn@gmail.com>
2017-09-18 11:54:34 -0600

    Continued improvments to parsing

--- polytech_data/main.py
+++ polytech_data/main.py
@@ -6,36 +6,38 @@ import render
 import numpy as np
 from tabulate import tabulate
 
-
+EXCEPTIONS=True
 RENDER_LABELS = True
 ID2_X_DISPLACEMENT = "Response X  Displacement"
 ID2_Y_DISPLACEMENT = "Response Y  Displacement"
 ID2_Z_DISPLACEMENT = "Response Z  Displacement"
 
 
-#Class which holds the initial position and all displacements
-#of a single vertex
+#Class which holds the initial position and id of a nodeclass NodeData(object):
 class NodeData(object):
-    #Gets node data out of given uff node entry
-    def __init__(self, uff_nodes, node_id):
+    #Gets a single node's data out of given uff node entry, or None if it does not exist
+    def for_label(uff_nodes, node_label):
+        for label, coord in zip(uff_nodes.labels, uff_nodes.coords):
+            if label == node_label:
+                return NodeData(node_label, coord)
+        if EXCEPTIONS:
+            raise Exception("Could not find NodeData for node_id={}".format(node_id))
+        else:
+            return None
+            
+    def __init__(self, label, position):
         #Store id
-        self.id = node_id
-
-        #Get position, exit on success
-        for label, coord in zip(d.labels, d.coords):
-            if label == target_node_id:
-                self.postion = coord
-                return
-
-        #If reach this position, failed to find position
-        raise Exception("Unable to find node id {} in uff_nodes".format(node_id))
+        self.label = label
+        self.position = position
 
 
+#Class which holds all displacement data of a single node
 class DisplacementData(object):
-    def for_node(data_set, node_id):
+    #Takes the entire uff data set and scans for displacement data relevant to this node
+    def for_label(data_set, node_label):
         x,y,z = None,None,None
         for d in data_set:
-            if isinstance(d, uff.UffFunctionAtNode) and d.response["node"] == node_id:
+            if isinstance(d, uff.UffFunctionAtNode) and d.response["node"] == node_label:
                 id2 = d.ids[1]
                 if      id2 == ID2_X_DISPLACEMENT:
                     x = d
@@ -45,11 +47,15 @@ class DisplacementData(object):
                     z = d
 
         if not any(e is None for e in (x,y,z)):
-            return DisplacementData(x,y,z)
+            return DisplacementData(node_label, x,y,z)
         else:
-            return None
+            if EXCEPTIONS:
+                raise Exception("Could not find DisplacementData for node_id={}".format(node_id))
+            else:
+                return None
 
-    def __init__(self, uff_x_func, uff_y_function, uff_z_function):
+    #Creates displacement data object out of the given function data
+    def __init__(self, node_label, uff_x_func, uff_y_function, uff_z_function):
         try:
             #Get displacements from each func set
             disp_x = uff_x_func.axis[uff.AXIS_ORDINATE]["data"]
@@ -57,14 +63,34 @@ class DisplacementData(object):
             disp_z = uff_z_func.axis[uff.AXIS_ORDINATE]["data"]
             disp_times = uff_z_func.axis[uff.AXIS_ABSCISSA]["data"] #z is arbitrary 
 
+            self.label = node_label
             self.disp_vecs = [np.array([x,y,z], dtype=np.float32) for x,y,z in zip(disp_x, disp_y, disp_z)]
             self.disp_times = disp_times
         except AttributeError:
             raise Exception("Invalid uff data provided to DisplacementData")
 
-
+#Class which holds a single "Element" in terms of constructed geometry
 class ElementData(object):
-    def __init__(self, uff_elements):
+    def for_label(uff_elements, element_label):
+        #Find the label
+        i = uff_elements.labels.index(element_label)
+        if i == -1:
+            if EXCEPTIONS:
+                raise Exception("Could not find Element with id={}".format(element_id))
+            else:
+                return None
+
+        #Yield
+        label = uff_elements.labels[i]
+        color = uff_elements.colors[i]
+        nodes = uff_elements.elements[i]
+        return ElementData(label, color, nodes)
+            
+                            
+    def __init__(self, label, color, node_labels): 
+        self.label = label
+        self.color = color
+        self.nodes = node_labels
         
 
 #Draws all node objects
@@ -92,9 +118,16 @@ def main(filename):
         #If UffNodes, build node_data
         if isinstance(d, uff.UffNodes):
             for label in d.labels:
-                node_data[label] = NodeData(d, label)
+                node_data[label] = NodeData.for_label(d, label)
 
-        #If 
+        #If UffElements, build element_data
+        if isinstance(d, uff.UffElements):
+            for label in d.labels:
+                element_data[label] = ElementData.for_label(label)
+
+    #Once have nodes, gather specific data from them
+    for node_label in node_data.keys():
+        displacement_data[node_label] = DisplacementData.for_label()
 
     node_data = [NodeData(data_set, node_num) for node_num in range(1,25)]
     element_data = [ElementData(data_set
